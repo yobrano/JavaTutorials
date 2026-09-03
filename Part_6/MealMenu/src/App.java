24d31fbe7d248e55e1a9ee71f46c98a81ba13fd3
yobrano <mburubrian2@gmail.com>
2026-09-03 04:11:19 +0300

    Menu - Objects containing list of objects

---
+++ Part_6/MealMenu/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Menu menu = new Menu();
+        menu.addMeal("Tofu ratatouille");
+        menu.addMeal("Chilli coconut chicken");
+        menu.addMeal("Chilli coconut chicken");
+        menu.addMeal("Meatballs with mustard sauce");
+
+        menu.printMeals();
+        menu.clearMenu();
+
+        System.out.println();
+        menu.addMeal("Tomato and mozzarella salad");
+        menu.printMeals();
+
+    }
+}
